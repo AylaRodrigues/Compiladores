cf66a14cb313f3d66c38bc7e8b3c5b4b3ad5d78b
Ayla Rodrigues <4ylarodrigues@gmail.com>
2022-09-27 12:29:39 -0300

    Update Sintatico.py

--- Analisador Sintatico/Sintatico.py
+++ Analisador Sintatico/Sintatico.py
@@ -1,41 +1,41 @@
 import ply.yacc as yacc
 
-from calclex import tokens
+from Lexer import m
 
- def p_program(p):
- 	'program : class ponto_virgula'
- 	p[0] = p[1]
+def p_program(p):
+    'program : class ponto_virgula'
+    p[0] = p[1]
 
  #Perguntar como colocar partes opcionais
- def p_class(p):
- 	'CLASS ID INHERITS ID abre_chaves fecha_chaves'
+def p_class(p):
+    'CLASS ID INHERITS ID abre_chaves fecha_chaves'
 
- def p_formal(p):
- 	'formal : ID dois_pontos ID'
- 	p[0] = p[1] : p[3]
+def p_formal(p):
+    'formal : ID dois_pontos ID'
+    p[0] = p[1] : p[3]
 
- def p_expr_seta(p):
- 	'expr : ID seta ID'
- 	p[0] = p[1] <- p[3]
+def p_expr_seta(p):
+    'expr : ID seta ID'
+    p[0] = p[1] <- p[3]
 
- #Perguntar como proceder com if, then, ..., fi
- def p_expr_if(p):
- 	'expr : IF expr THEN expr ELSE expr FI'
- 	p[0] = p[1] if p[3] then p[5] else p[7] fi
+#Perguntar como proceder com if, then, ..., fi
+def p_expr_if(p):
+    'expr : IF expr THEN expr ELSE expr FI'
+    p[0] = p[1] if p[3] then p[5] 'else' p[7] fi
 
- def p_expr_new(p):
- 	'expr : NEW expr'
- 	p[0] = 
+def p_expr_new(p):
+    'expr : NEW expr'
+    p[0] =
 
- def p_expr_true(p):
- 	'expr : TRUE'
- 	p[0] = True
+def p_expr_true(p):
+    'expr : TRUE'
+    p[0] = True
 
- def p_expr_false(p):
- 	'expr : FALSE'
- 	p[0] = False
+def p_expr_false(p):
+    'expr : FALSE'
+    p[0] = False
 
- def p_expr_bin(p):
+def p_expr_bin(p):
      '''expr : expr mais expr
         	 | expr menos expr
         	 : expr multi expr
@@ -58,7 +58,6 @@ from calclex import tokens
      elif p[2] == '=':
          p[0] = p[1] = p[3]
 
-
  def p_expr_menos(p):
      'expr : expr menos expr'
      p[0] = p[1] - p[3]
@@ -70,3 +69,18 @@ from calclex import tokens
  def p_expr_dividir(p):
      'expr : expr dividir expr'
      p[0] = p[1] / p[3]
+
+def p_error(p):
+    print("Erro sintático!")
+
+# Build the parser
+parser = yacc.yacc()
+
+while True:
+   try:
+       s = input('calc > ')
+   except EOFError:
+       break
+   if not s: continue
+   result = parser.parse(s)
+   print(result)
