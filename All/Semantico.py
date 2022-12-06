45f0183b704b8a6dcd2a8c2373715a00c5ec53e9
SofiaKitaeva <75208744+SofiaKitaeva@users.noreply.github.com>
2022-12-06 14:30:33 -0300

    Update Semantico.py

--- All/Semantico.py
+++ All/Semantico.py
@@ -2,12 +2,12 @@ from Sintatico import arvore
 import copy
 
 #lista de tipos organizadas de forma (<nome>, <de quem herda>, <metodos>, <atributos>)
-TypeList = [('Object', None, [('abort', [], 'Object'), ('type_name', [], 'String'), ('copy', [], 'SELF_TYPE')], []),
-        ('SELF_TYPE', None, [], []),
-        ('IO', 'Object', [('out_string', [('x', 'String')], 'SELF_TYPE'), ('out_int', [('x', 'Int')], 'SELF_TYPE'), ('in_string', [], 'String'), ('in_int', [], 'Int')], []),
-        ('Int', 'IO', [], []),
-        ('String', 'IO', [('length', [], 'Int'), ('concat', [('s', 'String')], 'String'), ('substr', [('i', 'Int'), ('l', 'Int')], 'String')], []),
-        ('Bool', 'IO', [], [])]
+TypeList = [('Object', None, [('abort', [], 'Object'), ('type_name', [], 'String'), ('copy', [], 'SELF_TYPE')], [('self', 'Object')]),
+            ('SELF_TYPE', None, [], []), ('IO', 'Object', [('out_string', [('x', 'String')], 'SELF_TYPE'), ('out_int', [('x', 'Int')], 'SELF_TYPE'), ('in_string', [], 'String'),
+    ('in_int', [], 'Int')], []), ('Int', 'IO', [], []), ('String', 'IO',
+[('length', [], 'Int'), ('concat', [('s', 'String')], 'String'), ('substr', [('i', 'Int'), ('l', 'Int')], 'String')],
+[]), ('Bool', 'IO', [], [])]
+
 MethodsList = []
 IDsList = []
 
